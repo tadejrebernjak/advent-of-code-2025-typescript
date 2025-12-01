f029d7b75cf50f3dfb93d51ae9ed5a55ae94ff88
tadej <tadej.rebernjak2@gmail.com>
2025-12-01 19:14:39 +0100

    environment setup

--- src/utils/number.ts
+++ src/utils/number.ts
@@ -1,13 +1,11 @@
-export namespace NumberUtils {
-  export const avg = (a: number, b: number): number => {
-    return (a + b) / 2;
-  };
+export const avg = (a: number, b: number): number => {
+  return (a + b) / 2;
+};
 
-  export const mod = (number: number, mod: number): number => {
-    return ((number % mod) + mod) % mod;
-  };
+export const mod = (number: number, mod: number): number => {
+  return ((number % mod) + mod) % mod;
+};
 
-  export const isInt = (num: number): boolean => {
-    return num % 1 === 0;
-  };
-}
+export const isInt = (num: number): boolean => {
+  return num % 1 === 0;
+};
