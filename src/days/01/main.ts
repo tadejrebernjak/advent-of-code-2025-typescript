5ada93b4ea0aeea154359838674e9cb275c9d288
tadej <tadej.rebernjak2@gmail.com>
2025-12-01 19:14:50 +0100

    day 1

--- src/days/01/main.ts
+++ src/days/01/main.ts
@@ -1,12 +1,69 @@
-import type { Answer, Input, Solution } from "@/types/base.js";
+import type { Answer, Input, Solution } from "@/types/base";
+import { mod } from "@/utils/number";
 
-const _parseInput = (input: Input) => {
-  return input;
+const solvePart2 = (input: Input) => {
+  let position = 50;
+  let count = 0;
+
+  for (const line of input) {
+    const oldPosition = position;
+
+    const direction = line[0];
+    const steps = +line.slice(1);
+
+    const loops = Math.floor(steps / 100);
+    count += loops;
+
+    const effectiveSteps = mod(steps, 100);
+
+    if (direction === "R") {
+      position += effectiveSteps;
+
+      if (position > 99) {
+        count++;
+      }
+    } else {
+      position -= effectiveSteps;
+
+      if (position <= 0 && oldPosition !== 0) {
+        count++;
+      }
+    }
+
+    position = mod(position, 100);
+  }
+
+  return count;
 };
 
-export const main: Solution = (_input: Input): Answer => {
-  const part1 = null;
-  const part2 = null;
+const solvePart1 = (input: Input) => {
+  let position = 50;
+  let count = 0;
+
+  for (const line of input) {
+    const direction = line[0];
+    const steps = +line.slice(1);
+    const effectiveSteps = mod(steps, 100);
+
+    if (direction === "R") {
+      position += effectiveSteps;
+    } else {
+      position -= effectiveSteps;
+    }
+
+    position = mod(position, 100);
+
+    if (position === 0) {
+      count++;
+    }
+  }
+
+  return count;
+};
+
+export const main: Solution = (input: Input): Answer => {
+  const part1 = solvePart1(input);
+  const part2 = solvePart2(input);
 
   return {
     part1,
